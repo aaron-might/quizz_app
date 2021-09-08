b6d49e704720609fcf0ad9e3d08ccc049c5e8d1c
aaron-might <aaronmighty7@gmail.com>
2021-09-08 11:23:22 -0400

    create an array of objects with questions

---
+++ script.js
@@ -0,0 +1,41 @@
+const quizzData = [
+    {
+        question:'How old is Aaron?',
+        a: '12',
+        b: '18',
+        c: '190',
+        d: '1000',
+        correct:'d'
+    },
+    {
+        question: 'What isthe most used programming language in 2021?',
+        a:'Java',
+        b:'C#',
+        c:'JavaScript',
+        d:'Python',
+        correct:'a'
+    },
+    {
+        question: 'Who isthe President of the US?',
+        a:'Donald Trump',
+        b:'Ivan Saldano',
+        c:'Aaron Might',
+        d:'vladimir putin',
+        correct:'a'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a:'Hypertext Markup Language',
+        b:'Cascading Style Sheet',
+        c:'Helicopters Terminals Motorboats Lamborghini',
+        d:'Hyperlink Markup Language',
+        correct:'a'
+    },
+    {   question: 'What year was JavaScript launched ?',
+        a:'1996',
+        b:'1980',
+        c:'1999',
+        d:'none of the above',
+        correct:'a'
+    }
+];
